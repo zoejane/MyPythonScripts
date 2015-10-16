78590e74a22194a1f27aa45ccb4bd3722482aa9e
Zoe <dadac123@gmail.com>
2015-10-16 22:10:03 +0800

    open with chrome

--- open_webbraoser.py
+++ open_webbraoser.py
@@ -1,2 +1,14 @@
 import webbrowser
-webbrowser.open('http://docs.python.org/')
+
+url = 'http://docs.python.org/'
+
+# MacOS
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+# chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+
+webbrowser.get(chrome_path).open(url)
