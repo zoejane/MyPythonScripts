95dbfbd1f74c464665bacb049c0c906f8c823f87
Zoe <dadac123@gmail.com>
2015-10-17 10:14:24 +0800

    zhuzhou weather.com

--- weather.py
+++ weather.py
@@ -1,12 +1,16 @@
 import  pywapi
 import string
 
-weather_com_result = pywapi.get_weather_from_weather_com('10001')
-yahoo_result = pywapi.get_weather_from_yahoo('10001')
-noaa_result = pywapi.get_weather_from_noaa('KJFK')
+weather_com_result = pywapi.get_weather_from_weather_com('CHXX5650')
+#yahoo_result = pywapi.get_weather_from_yahoo('CHXX5650')
 
-print("Weather.com says: It is " + str.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n")
 
-print("Yahoo says: It is " + str.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n")
+print("Weather.com says: It is " + str.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Zhuzhou.\n\n")
 
-print("NOAA says: It is " + str.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n")
+#print("Yahoo says: It is " + str.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n")
+
+
+
+#a=pywapi.get_location_ids('zhuzhou')
+#print(a)
+
