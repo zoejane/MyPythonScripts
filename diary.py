1a84c60c1b6852ea2109769cb0a0ad84c04e1a06
Zoe <dadac123@gmail.com>
2015-10-18 21:56:44 +0800

    first diary ex

--- diary.py
+++ diary.py
@@ -1,16 +1,14 @@
 #! /usr/bin/env python3
 
 import sys
-diary=open('diary.txt','a')
+
 
 if len(sys.argv) >1:
-    content = ' '.join(sys.argv[1:])
-    if len(str(diary)==0):
-        diary.write(content)
-    else:
-        diary.write("\n" + content)
+    diary=open('diary.txt','a')
+    diary.write("\n" + content)
     
 else:
+
     content = diary.read()
     print(content)
 
