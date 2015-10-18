1663b28e64682b550f77616d72271f3cf98fa306
Zoe <dadac123@gmail.com>
2015-10-18 21:32:48 +0800

    add new line

--- diary.py
+++ diary.py
@@ -3,10 +3,11 @@ diary=open('diary.txt','a')
 
 if len(sys.argv) >1:
     content = ' '.join(sys.argv[1:])
-    diary.write(content)
+    diary.write("\n" + content)
     
 else:
     diary=open('diary.txt')
     content = diary.read()
+    print(content)
 
 diary.close()
